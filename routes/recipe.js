a377b28d33190b401561edbfc104b5d0ed118f5b
Mazen Zakarya <mazenzakarya222@gmail.com>
2026-04-10 17:13:40 +0200

    feat: initialize food app with Express and MongoDB setup
    - Add package.json with dependencies and scripts
    - Create recipe routes for CRUD operations
    - Implement server.js to set up Express server and connect to MongoDB

---
+++ routes/recipe.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const Recipe = require('../models/RecipeSchema');
+
+
+router.post('/', async (req, res) => {
+    const { title, ingredients, instructions } = req.body;
+    if (!title || !ingredients || !instructions) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+    const newRecipe = await Recipe.create({
+        title,
+        ingredients,
+        instructions
+    });
+    res.status(201).json(newRecipe);
+
+});
+router.get('/', async (req, res) => {
+    try{
+        
+        
+        const recipes = await Recipe.find();
+        res.status(200).json(recipes);
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+// Get a single recipe by ID
+router.get('/:id', async (req, res) => {
+    try{
+        const recipe = await Recipe.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({ message: 'Recipe not found' });
+        }
+        res.status(200).json(recipe);
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+// Update a recipe by ID
+router.put('/:id', async (req, res) => {
+    try{
+        const { title, ingredients, instructions } = req.body;
+
+        const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, { title, ingredients, instructions }, { new: true , runValidators: true});
+        if (!updatedRecipe) {
+            return res.status(404).json({ message: 'Recipe not found' });
+        }
+        res.status(200).json(updatedRecipe);
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+// Delete a recipe by ID
+router.delete('/:id', async (req, res) => {
+    try{
+        const recipe = await Recipe.findByIdAndDelete(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({ message: 'Recipe not found' });
+        }
+        res.status(200).json({ message: 'Recipe deleted' });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/connectionDB');
+
+connectDB();
+app.use('/api/recipes', require('./routes/recipe'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
